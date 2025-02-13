7e0d897764a6821df9fcdc503e1c852445b4d51c
CliffVille <Cliffordvill17@gmail.com>
2025-02-12 22:40:24 -0800

    Configured gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,25 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    compiler: {
-      styledComponents: true,
-    },
-    experimental: {
-      reactRefresh: false, // Ensure Fast Refresh is OFF
-    },
-    webpack(config) {
-      config.devServer = {
-        hot: false,  // Disable Hot Module Replacement
-        liveReload: false, // Disable live reload
-      };
-      return config;
-    },
-  };
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  experimental: {
+    reactRefresh: false, 
+  },
+  webpack(config) {
+    config.devServer = {
+      hot: false,  
+      liveReload: false, 
+    };
+    return config;
+  },
   
-  export default nextConfig;
-  
+  output: "export", 
+  basePath: "/wedding",
+  images: {
+    unoptimized: true, 
+  },
+};
+
+export default nextConfig;
