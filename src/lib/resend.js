933078cf14c15ae4156ad57d8a2ca263af71237c
CliffVille <Cliffordvill17@gmail.com>
2025-05-20 02:16:14 -0700

    still broken?

--- src/lib/resend.js
+++ src/lib/resend.js
@@ -1,5 +1,9 @@
 // lib/resend.js
 import { Resend } from 'resend';
 
-export const resend = new Resend(process.env.RESEND_API_KEY);
-    
+export function getResend() {
+  if (!process.env.RESEND_API_KEY) {
+    throw new Error("Missing RESEND_API_KEY environment variable");
+  }
+  return new Resend(process.env.RESEND_API_KEY);
+}
